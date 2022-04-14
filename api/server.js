1e0c45423e47782cb6e8a7640bf91fa1301401d2
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-14 14:54:28 -0700

    last thing i need to do is deploy the application on heroku

--- api/server.js
+++ api/server.js
@@ -1,10 +1,38 @@
+const e = require('express');
 const express = require('express'); 
+
 
 const User = require('../users/model'); 
 
 
 const server = express()
 server.use(express.json())
+// server.use(helmet()); 
+
+// server.get('/', (req, res) => {
+//     res.end('<h1> Hello, world!</h1>');
+// });
+
+
+server.get('/api/users', (req, res) => {
+    User.findAll()
+    .then(users => {
+        res.json(users)
+    })
+})
+
+server.post('./api/register'), (req, res) => {
+    let user = req.body; 
+    if(!user.username || !user.password){
+        res.status(404).json({message: "please provide user name and password"}); 
+    } else{
+        User.addUser(user)
+        .then(addedUser => {
+            res.status(200).json(addedUser); 
+        })
+    }
+}
+
 
 
 module.exports = server; 
